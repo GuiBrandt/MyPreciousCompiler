e89c9cc7f632c93249a0bc294b36b4e7d312a210
GuiBrandt <gui.g.brandt@gmail.com>
2017-12-03 16:30:58 -0200

    Agora tá lindo e maravilhoso

--- main.cpp
+++ main.cpp
@@ -1,25 +1,43 @@
 #include "include/Lexer.hpp"
-#include "include/PARSER.HPP"
+#include "include/Parser.hpp"
+
 #include <cstdlib>
+#include <cstring>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 /**
- * Teste do lexer
+ * Análise léxica
  */
-void lexerTest(const char* filename)
+void lexicalAnalysis(const char* filename)
 {
     try {
         Lexer l(filename);
 
-        printf("Code  Integer      Name\r\n\r\n");
+        printf(ANSI_COLOR_YELLOW "\r\nCode  Integer      Name" ANSI_COLOR_RESET "\r\n\r\n");
 
         while (l.hasMoreTokens())
         {
             TokenType tt = l.nextToken();
-            printf("0x%02x  0x%09x  %s\r\n", tt, l.getValue(), l.getName() != NULL ? l.getName() : "NULL");
+            printf(
+                ANSI_COLOR_GREEN "0x%02x  "
+                ANSI_COLOR_CYAN "0x%09x  "
+                ANSI_COLOR_YELLOW "%s"
+                ANSI_COLOR_RESET "\r\n",
+
+                tt, l.getValue(), l.getName() != NULL ? l.getName() : ANSI_COLOR_RED "<NULL>");
         }
     } catch (const char* e) {
-        fprintf(stderr, "Erro: %s", e);
+        fprintf(stderr, ANSI_COLOR_RED "Erro: %s" ANSI_COLOR_RESET, e);
     }
+
+    printf("\r\n");
 }
 
 /**
@@ -27,15 +45,70 @@ void lexerTest(const char* filename)
  */
 int main(int argc, char** argv)
 {
-    //lexerTest("teste.txt");
-    Parser p("teste.txt");
+    char* filename;
+
+    if (argc < 2 || argc > 3)
+    {
+        printf("Sintaxe: lotr [-l|--lex] <nome do arquivo>");
+        printf("\r\n\r\n");
+        printf("Se especificada a flag -l ou --lex, o compilador mostra também");
+        printf(" a análise léxica feita do código.");
+
+        return 0;
+    } else if (argc == 2) {
+
+        if (strcmp(argv[1], "-l") == 0 || strcmp(argv[1], "--lex") == 0)
+        {
+            fprintf(stderr, ANSI_COLOR_RED "Arquivo não foi especificado." ANSI_COLOR_RESET);
+            return -1;
+        } else
+            filename = argv[1];
+    } else {
+        if (strcmp(argv[1], "-l") != 0 && strcmp(argv[1], "--lex") != 0)
+        {
+            fprintf(stderr, ANSI_COLOR_RED "Opção desconhecida `%s'" ANSI_COLOR_RESET, argv[1]);
+            return -1;
+        }
+
+        filename = argv[2];
+        lexicalAnalysis(filename);
+    }
 
     try {
-        p.compileProgram();
+        Parser p(filename);
 
-        printf("Programa compilado com sucesso!");
+        try {
+
+            p.compileProgram();
+
+            printf(ANSI_COLOR_GREEN "Programa compilado com sucesso!" ANSI_COLOR_RESET "\r\n");
+
+        } catch (const char* e) {
+            fprintf(stderr, ANSI_COLOR_RED "Linha %d, Coluna %d: %s" ANSI_COLOR_RESET "\r\n\r\n", p.currentLine(), p.currentColumn(), e);
+
+            FILE* file = fopen(filename, "r");
+
+            int line = 0;
+            while (line < p.currentLine())
+                if (fgetc(file) == '\n') line++;
+
+            char chr;
+            while ((chr = fgetc(file)) != '\n')
+                fprintf(stderr, "%c", chr);
+
+            int i;
+            for (i = 0; i < p.currentColumn(); i++)
+                fprintf(stderr, " ");
+
+            fprintf(stderr, "^\r\n");
+
+            return -2;
+        }
+
     } catch (const char* e) {
-        fprintf(stderr, "Linha %d, Coluna %d: %s", p.currentLine(), p.currentColumn(), e);
+        fprintf(stderr, e);
+
+        return -3;
     }
 
     return 0;
